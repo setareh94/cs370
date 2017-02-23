538007218d84e5382e4498e8d5bc8358027ff387
Setareh Lotfi <setarehlotfi@Setarehs-MacBook-Air.local>
2017-02-23 13:15:20 -0800

    complete readline

--- p01/pa01.py
+++ p01/pa01.py
@@ -5,7 +5,7 @@ from collections import defaultdict
 numberOfStates = 0
 alphabets = []
 states = {} 
-transition = defaultdict(set)
+transition = {}
 startState = 0
 acceptingStates = []
 inputs = []
@@ -22,20 +22,21 @@ def readFile(s):
 	m = f.readline()
 	while( "'" in m):
 		a,b,c = m.split()
-		b = b.split("'")
-		transition[(a, b)] = c
+		b = b[1]
+		transition[tuple((a,b))] = c
 		m = f.readline()
 	startState = m
 	acceptingStates = f.readline().split()
 	l = f.readline()
 	while l:
-		inputs.add(m)
+		inputs.append(l.strip("\n"))
+		print(inputs)
 		l = f.readline()
 
 
 if __name__ == '__main__':
 	readFile('dfa1.txt')
-	print("Alphabet is %s \n transition %s \n states are %s: \n inputs: %s \n" %(alphabets,transition,acceptingStates,inputs))  
+	print("Alphabet is %s \n transition %s \n inputs: %s " %(alphabets,transition,inputs))  
 
 	print(states)
 	print(transition)
