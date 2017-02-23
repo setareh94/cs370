d6ca87a40b8d6f9174c191172a8f8d9f278c7567
Katie Levy <klevy@sandiego.edu>
2017-02-23 12:47:08 -0800

    read in file

--- p01/pa01.py
+++ p01/pa01.py
@@ -1,10 +1,14 @@
 import sys
 import os
+from collections import defaultdict
 
 numberOfStates = 0
 alphabets = []
 states = {} 
-transition = {}
+transition = defaultdict(set)
+startState
+acceptingStates = []
+inputs = []
 def readFile(s):
 	f = open(s,'w')
 
@@ -13,16 +17,16 @@ def readFile(s):
 	alphabets = list(x)
 	m = f.readline()
 	while( "'" in m):
-
 		a,b,c = m.split()
 		b = b.split("'")
-
-
-
-
-		
+		transition[(a, b)] = c
 		m = f.readline()
-
+	startState = m
+	acceptingStates = f.readline().split()
+	l = f.readline()
+	while l
+		inputs.add(m)
+		l = f.readline()
 
 
 
