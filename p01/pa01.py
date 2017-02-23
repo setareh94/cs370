ac84f7f3fe0e66753624810dee71346ec1575af6
Katie Levy <klevy@sandiego.edu>
2017-02-23 13:52:50 -0800

    fix bugs

--- p01/pa01.py
+++ p01/pa01.py
@@ -4,7 +4,6 @@ from collections import defaultdict
 
 numberOfStates = 0
 alphabets = []
-states = {} 
 transition = {}
 acceptingStates = []
 inputs = []
@@ -14,6 +13,7 @@ global startState
 
 def readFile(s):
 	global startState
+	global acceptingStates
 	f = open(s,'r')
 
 	numberOfStates = f.readline()
@@ -25,36 +25,30 @@ def readFile(s):
 		a,b,c = m.split("'")
 		b = b.strip()
 		c = c.strip(" ")
-		transition[tuple((int(a),int(b)))] = c.strip("\n")
+		transition[tuple((int(a),b))] = c.strip("\n")
 		m = f.readline()
 	startState = int(m.strip("\n"))
-	print("here %s",startState)
 
 	acceptingStates = f.readline().split()
 	l = f.readline()
 	while l:
 		inputs.append(l.strip("\n"))
-		print(inputs)
 		l = f.readline()
 
 def DFA(inputs):
 	currentState = startState
 
 	for val in inputs:
-		nextState = transition[tuple((int(currentState), int(val)))]
+		nextState = transition[tuple((int(currentState), val))]
 		currentState = nextState
-		print(currentState)
 	if currentState in acceptingStates:
 		print("Accept")
 		return
 	print("Reject")
 
 if __name__ == '__main__':
-	readFile('dfa1.txt')
-	print("Alphabet is %s \n transition %s \n inputs: %s " %(alphabets,transition,inputs))  
-
-	print(states)
-	print(transition)
+	readFile(sys.argv[1])
+	print("Alphabet is %s \n transition %s \n inputs: %s " %(alphabets,transition,inputs))
 	for i in inputs:
 		DFA(i)
 
