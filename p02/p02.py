ca9e2b0fdffac3b3114cc9c4e79d3fc6420bf6c2
setareh94 <setarehlotfi94@hotmail.com>
2017-04-28 18:04:05 -0700

    latest version

--- p02/p02.py
+++ p02/p02.py
@@ -1,6 +1,8 @@
-# COMP 370, Spring 2017
-# Program #2, DFA Simulation
-# Co-Authored: Setareh Lotfi and Katie Levy
+"""
+ COMP 370, Spring 2017
+ Program #2, NFA to DFA Conversion
+ Co-Authored: Setareh Lotfi and Katie Levy
+"""
 
 #System library for read and write
 import sys
@@ -9,67 +11,78 @@ import os
 from collections import defaultdict
 from collections import deque
 
+# Global variables for easier transitons
 numberOfStates = 0
-alphabets = []
-transition = defaultdict()
-acceptingStates = []
-inputs = []
-startStateAfterE = None
-inputs = []
-newDFATransition = {}
-newStartState = None
-newAcceptStates = set()
+alphabets = []          # List of alphabets
+transition = defaultdict() 
+acceptingStates = []	# List of accepting states
+inputs = [] 			# List of DFA Inputs
+startStateAfterE = None # Start state after epsilon transition
+newDFATransition = {}   # Map for DFA transition
+newStartState = None    # new start state for DFA
+newAcceptStates = set() #set of accepting states
 
-global startState
 
-# Open a file, s, and parse through the file to
-# properly assign global variables for the NFA to 
-# DFA conversion
-
-def readFile(s):
+"""
+Function: readFile
+Arguments: fileName
+Description:
+	Open the file, fileName, and parse through the file to
+	properly assign global variables for the NFA to 
+	DFA conversion
+"""
+def readFile(fileName):
 
 	global startState
 	global acceptingStates
 	global alphabets
-	f = open(s,'r')
+	global numberOfStates
 
-	numberOfStates = f.readline()
-	x = f.readline()
-	alphabets = list((x.strip("\n")))
+	with open(fileName,'r') as f:
 
-	# Read through all the transitions
-	# and add them to the transition dictionary
-	# with the state and input as the key mapping to
-	# the resulting state
-	m = f.readline()
-	while( "'" in m):
-		a,b,c = m.split("'")
-		b = b.strip()
-		c = c.strip(" ")
-		if tuple((int(a),b)) in transition:
-			transition[tuple((int(a),b))].append(c.strip("\n"))
-		else:
-			transition[tuple((int(a),b))] = [c.strip("\n")]
+		numberOfStates = (f.readline()).strip("\n")
+		x = f.readline()
+		alphabets = list((x.strip("\n")))
+
+		# Read through all the transitions
+		# and add them to the transition dictionary
+		# with the state and input as the key mapping to
+		# the resulting state
 		m = f.readline()
 
-		
-	m = f.readline()
-	#Store StartState from the file
-	startState = m.strip("\n")
+		while( "'" in m):
+			a,b,c = m.split("'")
+			b = b.strip()
+			c = c.strip(" ")
+			if tuple((int(a),b)) in transition:
+				transition[tuple((int(a),b))].append(c.strip("\n"))
+			else:
+				transition[tuple((int(a),b))] = [c.strip("\n")]
+			m = f.readline()
+			
+		m = f.readline()
+		#Store StartState from the file
+		startState = m.strip("\n")
 
-	#Store acceptionStates
-	acceptingStates = list((map(int, f.readline().split())))
+		#Store acceptionStates
+		acceptingStates = list((map(int, f.readline().split())))
 
-# Convert the NFA to a DFA with a
-# new transition function and new states
-# keeping track of what states the NFA would be at
+
+"""
+Function: conversionToDFA
+Arguments: none
+Description:
+	Convert the NFA to a DFA with a
+	new transition function and new states
+	keeping track of what states the NFA would be at
+"""
 def conversionToDFA():
-	print("Transtion is %s \n" % transition)
 	DFATransitions = defaultdict()
 	newStatesMarked = list()
 	queue = deque()
 	findNewStartState()
 	start = list(startStateAfterE)
+
 	if start not in queue:
 		queue.append(start)
 	while (queue):
@@ -86,39 +99,31 @@ def conversionToDFA():
 							nextStates.append(x)
 			# Check what state the next states could be in 
 			# with the episilon transitions
-			checkForE = deque()
-			checkForE.append(nextStates + currentState)
-			checkedForE = list()
-			while (checkForE):
-				curState = checkForE.popleft()
-				for eachState in curState:
-					if eachState not in sorted(checkedForE):
-						checkedForE.append(eachState)
-						v = tuple((int(eachState), "e"))
-						if v in transition:
-							for x in transition[v]:
-								if x not in nextStates:
-									nextStates.append(x)
-									if (x not in sorted(checkForE)) and (x not in sorted(checkedForE)):
-										checkForE.append(x)
-
+			epsilonTransition(nextStates, currentState)
 			# Add to the new transition function
 			nextStates = sorted(nextStates)
 			DFATransitions[tuple((tuple(currentState,), a))] = nextStates
 
-			if (sorted(nextStates) not in queue) and (sorted(nextStates) not in newStatesMarked) and sorted(nextStates):
+			if ((sorted(nextStates) not in queue) 
+				and (sorted(nextStates) not in newStatesMarked)
+				and sorted(nextStates)):
 				queue.append(sorted(nextStates))
-	print("Printing DFA Transitions")
-	print(DFATransitions)
+	#Covert from set of one states to set of integers
 	changeStateNames(DFATransitions)
 
-# Find the new start state compensating for epsilon transitions
-def findNewStartState():
-	global startStateAfterE
+
+"""
+Function: epsilonTransition
+Arguments: nextstates, currentState
+Description:
+ 	The function Check what state the next states could be in 
+	with the episilon transitions
+"""
+def epsilonTransition(nextStates, currentState):
 	checkForE = deque()
-	checkForE.append(startState)
+	checkForE.append(nextStates + currentState)
 	checkedForE = list()
-	nextStates = list(startState)
+
 	while (checkForE):
 		curState = checkForE.popleft()
 		for eachState in curState:
@@ -131,9 +136,46 @@ def findNewStartState():
 							nextStates.append(x)
 							if (x not in sorted(checkForE)) and (x not in sorted(checkedForE)):
 								checkForE.append(x)
+
+
+"""
+Function: findNewStartState
+Arguments: none
+Description:
+	Find the new start state compensating
+	for epsilon transitions
+"""
+def findNewStartState():
+	global startStateAfterE
+	checkForE = deque()
+	checkForE.append(startState)
+	checkedForE = list()
+	nextStates = list(startState)
+
+	while (checkForE):
+		curState = checkForE.popleft()
+		for eachState in curState:
+			if eachState not in sorted(checkedForE):
+				checkedForE.append(eachState)
+				v = tuple((int(eachState), "e"))
+				if v in transition:
+					for x in transition[v]:
+						if x not in nextStates:
+							nextStates.append(x)
+							if (x not in sorted(checkForE)) and (x not in sorted(checkedForE)):
+								checkForE.append(x)
+
 	startStateAfterE = sorted(nextStates)
 
-# Convert DFA states from the form of set of states to just one integer
+
+"""
+Function: changeStateNames
+Arguments: DFATransitions
+Description:
+	Takes in a dictionary containing DFA transition 
+	functions (DFATransitions) and Convert DFA states 
+	from the form of set of states to just one integer.
+"""
 def changeStateNames(DFATransitions):
 	lookup = {}
 	i = 1
@@ -142,12 +184,8 @@ def changeStateNames(DFATransitions):
 	global newAcceptStates
 	# Go through every transition states
 	for key, inp in DFATransitions:
-		print("\n Printing lookup\n %s \n key %s \n, \n input %s \n" %(lookup,key,inp))
 		value = DFATransitions[key, inp]
-		print(value)
 		if key in lookup:
-			print("inside if")
-			print(key)
 			newKey = tuple((lookup[key], inp))
 		else:
 			lookup[key] = i
@@ -160,39 +198,66 @@ def changeStateNames(DFATransitions):
 			newValue = i
 			i = i +1
 		newDFATransition[newKey] = newValue
-		print("printing new DFA Transition")
-		print(newDFATransition)
+
 	newStartState = lookup[tuple(startStateAfterE,)]
-	print(newStartState)
+
 	for keys in lookup:
 		for s in acceptingStates:
 			if (str(s) in keys):
 				newAcceptStates.add(lookup[keys])
-	print("new accepting states")
-	print(newAcceptStates)
 
 
-#Read the DFAinput files and store them for output
-def readFileForDFAInput(s):
-	file = open(s, 'r')
-	nextLine = file.readline()
-	while nextLine:
-		inputs.append(nextLine.strip("\n"))
+"""
+ Function: writeDFAtoFile
+ Arguments: fileName
+ Description:
+	Takes in filename and write the created dfa 
+	into a user specific file.
+"""
+
+def writeDFAtoFile(filename):
+	with open(filename, 'w') as f:
+		f.write(str(numberOfStates) + '\n')
+		f.write("".join(i for i in alphabets) + '\n')
+		result = "\n".join(str(key[0]) + " '" + key[1] + "' " + str(value) 
+							for key, value in newDFATransition.items())
+		f.write(result + '\n')
+		f.write(str(newStartState) + '\n')
+		f.write(" ".join(str(i) for i in newAcceptStates))
+
+
+"""
+ Function: readFileForDFAInput
+ Arguments: fileName
+ Description:
+	Takes in filename and read the DFAinput files 
+	and store them for output.
+"""
+def readFileForDFAInput(fileName):
+	with open(fileName, 'r') as file:
 		nextLine = file.readline()
+		while nextLine:
+			inputs.append(nextLine.strip("\n"))
+			nextLine = file.readline()
+ 
 
-# Run through the inputs based on the DFA created
-# and output accept if the input is in the language
-# or output reject if the input is not in the language
+"""
+ Function: checkDFAInput
+ Arguments: none
+ Description:
+	Run through the inputs based on the DFA created
+	and prints accept if the input is in the language
+	otherwise prints reject if the input is not in
+	the language.
+"""
 def checkDFAInput():
 	currentState = newStartState
-	print("accepting states")
-	print(acceptingStates)
+
 	if (inputs) :
-		print("printing inputs")
-		print(inputs)
 		for val in inputs:
 			currentState = newStartState
-			#empty strings should be accepted only if the accepting state was same as the current sate
+			#empty strings should be accepted only
+			# if the accepting state was same as the current sate
 			if len(val) == 0:
 				if currentState in newAcceptStates:
 					print("Accept")
@@ -204,6 +269,7 @@ def checkDFAInput():
 					try:
 						nextState = newDFATransition[tuple((int(currentState), i))]
 						currentState = int(nextState)
+					#If the transition path did not exist reject
 					except KeyError:
 						reject = True
 
@@ -212,9 +278,27 @@ def checkDFAInput():
 				else:
 					print("Reject")
 
-# Main function
+
+"""
+Main function
+"""
 if __name__ == '__main__':
-	readFile(sys.argv[1])
-	readFileForDFAInput(sys.argv[2])	
-	conversionToDFA()
-	checkDFAInput()
+	# Guide the user on the program usage
+	if len(sys.argv) == 4:
+		print ("Usage: \nArgument 1: NFA text file (e.g: NFA1.txt) \n"
+				+ "Argument 2: Desired file name to write the equivalent DFA to (e.g: DFA1.txt)\n" + 
+				"Argument 3: DFA input file to evaluate the produced DFA (e.g: DFAinput.txt)")
+		readFile(sys.argv[1])
+		#Third argument takes in DFAinput text file
+		readFileForDFAInput(sys.argv[3])
+		conversionToDFA()
+		checkDFAInput()
+		#Second argument takes in file name to output produced DFA 
+		writeDFAtoFile(sys.argv[2])
+
+	else:
+		#First argument takes in NFA text file
+		readFile(sys.argv[1])
+		conversionToDFA()
+		#Second argument takes in file name to output produced DFA 
+		writeDFAtoFile(sys.argv[2])
