1c4115715cd989554fef7bf4c2b39aced9e36a3a
setareh94 <setarehlotfi94@hotmail.com>
2017-05-02 00:45:14 -0700

    more stuff

--- p03/p03.py
+++ p03/p03.py
@@ -1,3 +1,124 @@
+class Automata:
+    """class to represent an Automata"""
+
+    def __init__(self, language = set(['0', '1'])):
+        self.states = set()
+        self.startstate = None
+        self.finalstates = []
+        self.transitions = dict()
+        self.language = language
+
+    @staticmethod
+    def epsilon():
+        return ":e:"
+
+    def setstartstate(self, state):
+        self.startstate = state
+        self.states.add(state)
+
+    def addfinalstates(self, state):
+        if isinstance(state, int):
+            state = [state]
+        for s in state:
+            if s not in self.finalstates:
+                self.finalstates.append(s)
+
+    def addtransition(self, fromstate, tostate, inp):
+        if isinstance(inp, str):
+            inp = set([inp])
+        self.states.add(fromstate)
+        self.states.add(tostate)
+        if fromstate in self.transitions:
+            if tostate in self.transitions[fromstate]:
+                self.transitions[fromstate][tostate] = self.transitions[fromstate][tostate].union(inp)
+            else:
+                self.transitions[fromstate][tostate] = inp
+        else:
+            self.transitions[fromstate] = {tostate : inp}
+
+    def addtransition_dict(self, transitions):
+        for fromstate, tostates in transitions.items():
+            for state in tostates:
+                self.addtransition(fromstate, state, tostates[state])
+
+
+
+    def newBuildFromNumber(self, startnum):
+        translations = {}
+        for i in list(self.states):
+            translations[i] = startnum
+            startnum += 1
+        rebuild = Automata(self.language)
+        rebuild.setstartstate(translations[self.startstate])
+        rebuild.addfinalstates(translations[self.finalstates[0]])
+        for fromstate, tostates in self.transitions.items():
+            for state in tostates:
+                rebuild.addtransition(translations[fromstate], translations[state], tostates[state])
+        return [rebuild, startnum]
+
+
+
+
+class BuildAutomata:
+    """class for building e-nfa basic structures"""
+
+    @staticmethod
+    def basicstruct(inp):
+        state1 = 1
+        state2 = 2
+        basic = Automata()
+        basic.setstartstate(state1)
+        basic.addfinalstates(state2)
+        basic.addtransition(1, 2, inp)
+        return basic
+
+    @staticmethod
+    def plusstruct(a, b):
+        [a, m1] = a.newBuildFromNumber(2)
+        [b, m2] = b.newBuildFromNumber(m1)
+        state1 = 1
+        state2 = m2
+        plus = Automata()
+        plus.setstartstate(state1)
+        plus.addfinalstates(state2)
+        plus.addtransition(plus.startstate, a.startstate, Automata.epsilon())
+        plus.addtransition(plus.startstate, b.startstate, Automata.epsilon())
+        plus.addtransition(a.finalstates[0], plus.finalstates[0], Automata.epsilon())
+        plus.addtransition(b.finalstates[0], plus.finalstates[0], Automata.epsilon())
+        plus.addtransition_dict(a.transitions)
+        plus.addtransition_dict(b.transitions)
+        return plus
+
+    @staticmethod
+    def dotstruct(a, b):
+        [a, m1] = a.newBuildFromNumber(1)
+        [b, m2] = b.newBuildFromNumber(m1)
+        state1 = 1
+        state2 = m2-1
+        dot = Automata()
+        dot.setstartstate(state1)
+        dot.addfinalstates(state2)
+        dot.addtransition(a.finalstates[0], b.startstate, Automata.epsilon())
+        dot.addtransition_dict(a.transitions)
+        dot.addtransition_dict(b.transitions)
+        return dot
+
+    @staticmethod
+    def starstruct(a):
+        [a, m1] = a.newBuildFromNumber(2)
+        state1 = 1
+        state2 = m1
+        star = Automata()
+        star.setstartstate(state1)
+        star.addfinalstates(state2)
+        star.addtransition(star.startstate, a.startstate, Automata.epsilon())
+        star.addtransition(star.startstate, star.finalstates[0], Automata.epsilon())
+        star.addtransition(a.finalstates[0], star.finalstates[0], Automata.epsilon())
+        star.addtransition(a.finalstates[0], a.startstate, Automata.epsilon())
+        star.addtransition_dict(a.transitions)
+        return star
+
+
 class NFAfromRegex:
     """class for building e-nfa from regular expressions"""
 
@@ -87,6 +208,6 @@ class NFAfromRegex:
             a = self.automata.pop()
             b = self.automata.pop()
             if operator == self.plus:
-                self.automata.append(BuildAutomata.plusstruct(b,a))
+                self.automata.append(BuildAutomata.plusstruct(b,a)) 
             elif operator == self.dot:
-                self.automata.append(BuildAutomata.dotstruct(b,a))
+                self.automata.append(BuildAutomata.dotstruct(b,a)) 
